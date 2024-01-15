d433983f3fd4e98592e2503d34303a9996f2ea2f
Stephen Copplestone <stephen@copplestone.de>
2024-01-15 20:38:33 +0100

    Updated Read the Docs extensions

--- docs/documentation/conf.py
+++ docs/documentation/conf.py
@@ -44,7 +44,9 @@ extensions = [
         'myst_parser'
         ]
 
+# Set width
 sphinx_rtd_size_width='98%'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
